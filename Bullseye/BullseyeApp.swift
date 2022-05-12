a86fd85b33a87ad66fe0b64a23b64d8c94da202a
carolinechan17 <carolinechan304@gmail.com>
2022-05-12 16:30:02 +0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Caroline Chan on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
